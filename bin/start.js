b71002a9c5e7a4c2f95c7ecd937379b4178d0324
Yi-love <438002896@qq.com>
2016-11-17 22:18:12 +0800

    fix error

--- bin/start.js
+++ bin/start.js
@@ -6,9 +6,7 @@ var os = require('os');
 var ip = void 0,
     port = void 0;
 
-console.log('use :' , process.argv);
-
-if ( process.argv.length === 1 ) {
+if ( process.argv.length === 2 ) {
     var ipObj = os.networkInterfaces();
     for ( var ipClass in ipObj ){
         var arr = ipObj[ipClass];
@@ -19,11 +17,11 @@ if ( process.argv.length === 1 ) {
             }
         }
     }
-}else if ( process.argv.length === 2 ) {
-    ip = process.argv[1];
-}else if( process.argv.length === 3 ){
-    ip = process.argv[1];
-    port = process.argv[2];
+}else if ( process.argv.length === 3 ) {
+    ip = process.argv[2];
+}else if( process.argv.length === 4 ){
+    ip = process.argv[2];
+    port = process.argv[3];
 }else{
     console.log('arguments invalid. please use `dnsproxy[,ip[,port]]`');
 }
